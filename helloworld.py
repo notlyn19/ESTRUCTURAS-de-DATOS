16c8dc6fe7d5bc8f73cd5794c24ac66e9bed378b
notlyn19 <167796507+notlyn19@users.noreply.github.com>
2024-09-18 16:00:01 -0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+#1 seccion comments
+# #HOLA MUNDO PARA LA MATERIA O CURSO DE ESTRUCTURAS DE DATOS BASADO EN LENGUAJE PYTHON
+
+#2 seccion imprimir
+print("hello world")
+
+#1 seccion variables
+
+s="lyn"
+m=5
+
+print(s)
+print(m)
+
+"""
+los tipos de variables ilegales son:
+2myvar = "John"
+my-var = "John"
+my var = "John"
+"""
---
+++ variables3.1-3.2.py
@@ -0,0 +1,50 @@
+#3 seccion variables
+#3.1 variables de python
+
+x="lyn"
+y=5
+
+print(x)
+print(y)
+
+#fundicion
+ 
+a = str(3)    # a podria ser '3'
+b = int(3)    # b podria ser 3
+c = float(3)  # c podria ser 3.0
+
+print(a)
+print(b)
+print(c)
+
+#obtener el tipo de dato 
+
+a = 5
+b= "John"
+print(type(a))
+print(type(b))
+
+#comillas simples y dobles
+"""
+x = "John"
+son lo mismo
+x = 'John'
+"""
+#Distingue mayúsculas y minúsculas
+
+k="mora"
+K=5
+print(k)
+print(K)
+
+
+#3.2 nombres de variables 
+
+#los tipos de variables ilegales son:
+"""
+
+2myvar = "John"
+my-var = "John"
+my var = "John"
+
+"""
